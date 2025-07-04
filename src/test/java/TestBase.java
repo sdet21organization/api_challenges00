395cb98630df830b8dfdbe6d725e51dc9586b902
Alex Ov <alexov@swisscows.email>
2025-07-04 21:35:03 +0300

    Initial commit

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,19 @@
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.given;
+
+public class TestBase {
+
+    static String url = "https://apichallenges.herokuapp.com";
+
+    @BeforeAll
+    public static void setup(){
+        RestAssured.baseURI =url;
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
+
+}
